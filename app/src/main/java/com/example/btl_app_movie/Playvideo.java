5aad0fb4c933b71f38d2e96ef43aee00d33129d5
Thanhduc112200 <122009488+Thanhduc112200@users.noreply.github.com>
2023-12-19 14:22:29 +0700

    update database , playvideo, addintro

--- app/src/main/java/com/example/btl_app_movie/Playvideo.java
+++ app/src/main/java/com/example/btl_app_movie/Playvideo.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AccoutActivity extends AppCompatActivity {
+public class Playvideo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accout);
+        setContentView(R.layout.activity_playvideo);
     }
 }
